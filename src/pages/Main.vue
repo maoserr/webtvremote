d4db9d2a6d2a3149617e8e2f11e020e8c6e624c3
Maoserr <maoserr@gmail.com>
2024-07-13 14:32:36 -0500

    themes

--- src/pages/Main.vue
+++ src/pages/Main.vue
@@ -1,25 +1,15 @@
 <template>
-  <div class="grid" style="width:100%">
-    <div class="col-3">
-      <a href="https://www.twitch.com/directory/following">Twitch</a>
-    </div>
-    <div class="col-3">
-      <a href="https://www.youtube.com">Youtube</a>
-    </div>
-    <div class="col-3">
-      <a href="options.html">Options</a>
-    </div>
-    <div class="col-12">
-      {{ logs }}
-    </div>
-  </div>
+  <h3><img src="images/icon128.png"/>Web TV Remote</h3>
+  <ul>
+    <li><a href="https://www.twitch.com/directory/following">Twitch</a></li>
+    <li><a href="https://www.youtube.com">Youtube</a></li>
+    <li><a href="options.html">Options</a></li>
+  </ul>
 </template>
 
 <script setup lang="ts">
-import 'primeflex/primeflex.css';
 import 'primevue/resources/primevue.min.css';
 import 'primeicons/primeicons.css';
-import 'primevue/resources/themes/bootstrap4-light-blue/theme.css';
 import {onMounted, Ref, ref} from "vue";
 import {getSelEls} from "../services/selectable";
 
